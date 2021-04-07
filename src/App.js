c538d5ca4ade7ebb9159e431b75f13f2b15bf8ce
Javier Gonzalez <jgromeo93@gmail.com>
2021-04-07 13:46:35 +0200

    changes made foldernames and filenames by  javier

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Charts from "./componets/Budgetprec.js"; 
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <h1>Hello Budget-app</h1>
-=======
-   
->>>>>>> 0091a47ba47d7ef4e4b287b8f8eeb4eede717cae
+      <p>Testing</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,17 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-=======
-import reportWebVitals from './reportWebVitals';
-import Chart from "react-google-charts";
-// or
-import { Chart } from "react-google-charts";
-// or
-const { Chart } = require("react-google-charts");
-// or
-var Chart = require("react-google-charts").Chart;
->>>>>>> 0091a47ba47d7ef4e4b287b8f8eeb4eede717cae
 
 ReactDOM.render(
   <React.StrictMode>
